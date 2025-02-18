0c174dec9bef5dd43e2a3d61a2ace462fc295691
Kotaro <kota.kawaka@gmail.com_>
2025-02-18 14:57:25 +0700

    lab21.1 finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    double sum =0;
+    if(argc == 1){
+        cout << "Please input numbers to find average." << "\n";    
+    }
+    else{
+    cout << "---------------------------------" << "\n";
+
+    for(int i =0;i<argc;i++){
+        sum += atof(argv[i]);
+    }
+
+    cout << "Average of " <<argc-1<<" numbers = " <<sum/(argc-1) <<"\n";
+    cout << "---------------------------------";
+}
+}
